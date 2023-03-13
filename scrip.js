0a64a0c97e1ac53c56342614a4c0322661dead84
ojeda <ojeda2228@hotmail.com>
2023-03-13 10:46:48 -0600

    first commit

---
+++ scrip.js
@@ -0,0 +1,18 @@
+
+let menuVisible = false;
+function mostrarocultarmenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="resposive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+}
+
